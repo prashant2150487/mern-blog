be21f02fc654270f742af0a5820a0e128fd8cdf5
Prashnt Sachan <swepra04@gmail.com>
2024-05-25 15:50:19 +0530

    adde middleware function name error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'all fieled are required ' })
+        next(errorHandler(400, "All fields are required"))
     }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
@@ -13,9 +14,8 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json({ message: 'Signup succesful' })
 
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
-        })
+    } catch (error) {
+        next(error)
+
     }
 }
