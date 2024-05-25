de92af71e4a6bcc08165eb203c53f4db9c0055c4
Prashnt Sachan <swepra04@gmail.com>
2024-05-25 08:59:58 +0530

    databse is connected

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
-
-
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Databse is connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 app.listen(3000, () => {
-    console.log("server is listing in port : 3000")
-})
+    console.log("server is listing in port : 3000");
+});
