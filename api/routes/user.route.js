7ad33babec9f42f0a29db2e00b2e77d8d44614a0
Prashnt Sachan <swepra04@gmail.com>
2024-06-01 15:52:35 +0530

    update userupdate api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,14 @@
 
 import express from "express"
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
 import { signup } from "../controllers/auth.controller.js";
-
+import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 
 router.get('/test', test)
 router.post('/signup', signup)
+router.put('/update:userId', verifyToken, updateUser);
+
 
 export default router;
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -33,16 +33,6 @@ export default function DashProfile() {
     }, [imageFile]);
 
     const uploadImage = async () => {
-        // service firebase.storage {
-        //   match /b/{bucket}/o {
-        //     match /{allPaths=**} {
-        //       allow read;
-        //       allow write: if
-        //       request.resource.size < 2 * 1024 * 1024 &&
-        //       request.resource.contentType.matches('image/.*')
-        //     }
-        //   }
-        // }
         setImageFileUploadError(null);
         const storage = getStorage(app);
         const fileName = new Date().getTime() + imageFile.name;
